294a5bec309e3bf6445b94e91cbc2a8bd334cbf1
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2022-07-14 22:24:26 +0530

    Added solution - LeetHub

---
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find the maximum number of cuts.
+     int solve(int n, int& x, int& y, int& z,vector<int>& dp){
+        if(n<0)return INT_MIN;// if n<0 that means cut becomes greater then return INT_MIN
+        if(n==0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        
+        int choice1=1+solve(n-x,x,y,z,dp);
+        int choice2=1+solve(n-y,x,y,z,dp);
+        int choice3=1+solve(n-z,x,y,z,dp);
+        
+        dp[n]=max(choice1,max(choice2,choice3));
+        return dp[n];
+    }
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        vector<int> dp(n+1,-1);
+        int ans = solve(n,x,y,z,dp);
+        // cout<<INT_MIN<<endl;
+        return (ans<0 ? 0 : ans);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    
+    //taking testcases
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        //taking length of line segment
+        int n;
+        cin >> n;
+        
+        //taking types of segments
+        int x,y,z;
+        cin>>x>>y>>z;
+        Solution obj;
+        //calling function maximizeTheCuts()
+        cout<<obj.maximizeTheCuts(n,x,y,z)<<endl;
+
+    }
+
+	return 0;
+}  // } Driver Code Ends
