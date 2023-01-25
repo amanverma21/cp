a167f31b103fe2c0f17fe936263b59082592b18c
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-01-25 14:00:39 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -4,36 +4,37 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-    bool dfsCheck(int node, vector<int>adj[], int vis[],int pathVis[]){
-        vis[node]=1;
-        pathVis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfsCheck(it,adj,vis,pathVis)==true)
-                return true;
-            }
-            else if(pathVis[it]){
-                return true;
-            }
-        }
-        pathVis[node]=0;
-        return false;
-    }
   public:
     // Function to detect cycle in a directed graph.
-    // void dfs(int node,)
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        int vis[V]={0};
-        int pathVis[V]={0};
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfsCheck(i,adj,vis,pathVis)) 
-                return true;
-            }
-        }
-        return false;
+        int indegree[V]={0};
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i])
+	        indegree[it]++;
+	    }
+	    queue<int>q;
+	    for(int i=0;i<V;i++){
+	       if(indegree[i]==0)
+	        q.push(i);
+	    }
+	    int cnt=0;
+	   // vector<int>v;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        cnt++;
+	       // v.push_back(node);
+	    
+	    //to remove node from topo sort so to remove the indegree
+	    for(auto it:adj[node]){
+	        indegree[it]--;
+	        if(indegree[it]==0)
+	        q.push(it);
+	    }}
+	   // return v;
+	    if(V==cnt) return false;
+	    return true;
     }
 };
 
