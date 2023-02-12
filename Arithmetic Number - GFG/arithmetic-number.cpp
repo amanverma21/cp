d30fddeb2b74098a98184b58e2ad89418bfbfad2
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-02-13 00:11:10 +0530

    Added solution - LeetHub

--- Arithmetic Number - GFG/arithmetic-number.cpp
+++ Arithmetic Number - GFG/arithmetic-number.cpp
@@ -11,15 +11,18 @@ class Solution{
 public:
     int inSequence(int A, int B, int C){
         // code here
-        if(C==0&& (A==B)) return 1;
-        else if(C==0) return 0;
+        if(C==0&& (A==B))
+        return 1;
+        else if(C==0) 
+        return 0;
         if(C<0){
             if(B>A) return 0;
         }
         else{
             if(B<A) return 0;
         }
-        if((B-A)%C==0) return 1;
+        if((B-A)%C==0) 
+        return 1;
         return 0;
     }
 };
