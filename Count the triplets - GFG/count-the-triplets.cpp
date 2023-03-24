509261e6db59015aed6068d8ea7dd55fbf372adf
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-03-24 10:56:03 +0530

    Added solution - LeetHub

---
+++ Count the triplets - GFG/count-the-triplets.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int countTriplet(int arr[], int n)
+	{
+	    // Your code goes here
+	    unordered_set<int>st;
+	    for(int i=0;i<n;i++){
+            st.insert(arr[i]);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int el=arr[i]+arr[j];
+                if(st.find(el)!=st.end()) count++;
+            }
+        }
+        return count;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        	cin >> arr[i];
+
+        Solution ob;
+        cout << ob.countTriplet(arr, n) << "\n";
+
+    }
+
+    return 0;
+}
+// } Driver Code Ends
