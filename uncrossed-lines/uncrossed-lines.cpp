921f8cc38719f268736f1a12b3fba19b48bdd02b
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-07-03 12:50:07 +0530

    Time: 29 ms (34.35%) | Memory: 12.9 MB (60.51%) - LeetSync

--- uncrossed-lines/uncrossed-lines.cpp
+++ uncrossed-lines/uncrossed-lines.cpp
@@ -1,28 +1,20 @@
+//Memoisation
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int fun(int idx1,int idx2,int n,int m,vector<int>& nums1,vector<int>& nums2){
+        if(idx1==n or idx2==m) return 0;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        if(nums1[idx1]==nums2[idx2]){
+            return dp[idx1][idx2]=1+fun(idx1+1,idx2+1,n,m,nums1,nums2);
+        }
+        else{
+            return dp[idx1][idx2]=max(fun(idx1+1,idx2,n,m,nums1,nums2),fun(idx1,idx2+1,n,m,nums1,nums2));
+        }
+    }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size(),m=nums2.size();
-        // Dp for storing previous answers
-        int dp[max(n+1,m+1)][max(n+1,m+1)];
-        memset(dp,-1,sizeof(dp));
-        for(int i=0;i<=max(n,m);i++)
-        {
-            dp[0][i]=0;
-            dp[i][0]=0;
-        }
-        // loops for calculating answer for dp[i][j]
-        for(int i=1;i<=nums1.size();i++)
-        {
-            for(int j=1;j<=nums2.size();j++)
-          {
-            if(nums1[i-1]==nums2[j-1])
-            {
-                dp[i][j]=1+dp[i-1][j-1];
-            }
-            else
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-          }
-        }
-        return dp[n][m];
+        dp=vector<vector<int>>(n,vector<int>(m,-1));
+        return fun(0,0,n,m,nums1,nums2);
     }
 };
