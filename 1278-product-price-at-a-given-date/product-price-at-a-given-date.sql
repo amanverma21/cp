218688709060a1e39a93876f2fa2209819b46014
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-08-02 19:16:13 +0530

    Time: 888 ms (84.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,24 @@
+SELECT
+  product_id,
+  IFNULL (price, 10) AS price
+FROM
+  (
+    SELECT DISTINCT
+      product_id
+    FROM
+      Products
+  ) AS UniqueProducts
+  LEFT JOIN (
+    SELECT DISTINCT
+      product_id,
+      FIRST_VALUE (new_price) OVER (
+        PARTITION BY
+          product_id
+        ORDER BY
+          change_date DESC
+      ) AS price
+    FROM
+      Products
+    WHERE
+      change_date <= '2019-08-16'
+  ) AS LastChangedPrice USING (product_id);
