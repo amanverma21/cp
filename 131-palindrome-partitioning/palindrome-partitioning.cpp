9c6988bdba22ccc163c37925a8c8d18a5061805c
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-08-05 23:55:04 +0530

    Time: 65 ms (99.54%) | Memory: 49.2 MB (91.93%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    bool isPalindrome(string& s, int l, int r)
+    {
+        while(l < r)
+        {
+            if(s[l++] != s[r--])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(int idx, string& s, vector<string>& output, vector<vector<string>>& ans, int n)
+    {
+        // Base Case
+        if(idx == n)
+        {
+            ans.push_back(output);
+            return;
+        }
+        for(int i=idx; i<n; i++)
+        {
+            if(isPalindrome(s, idx, i))
+            {
+                output.push_back(s.substr(idx, i-idx+1));
+                solve(i+1, s, output, ans, n);
+                output.pop_back();
+            }
+        }
+    }
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> output;
+        solve(0, s, output, ans, s.size());
+        return ans;
+    }
+};
