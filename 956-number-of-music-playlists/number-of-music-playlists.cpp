88d8d8d651c60ab6954e72b4aacd945c5d8e11f7
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-08-06 19:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (46.85%) - LeetSync

---
+++ 956-number-of-music-playlists/number-of-music-playlists.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    long long solve(int n, int goal, int k, vector<vector<int>>&dp){
+        if(n==0&&goal==0) return 1;
+        if(n==0 || goal==0) return 0;
+        if(dp[n][goal]!=-1) return dp[n][goal];
+        long long first = solve(n-1, goal-1, k, dp)*n;
+        long long second = solve(n, goal-1, k, dp)*max(n-k,0);
+        return dp[n][goal]=(first+second)%MOD;
+    }
+    long long numMusicPlaylists(int n, int goal, int k) {
+        vector<vector<int>>dp(n+1, vector<int>(goal+1,-1));
+        return solve(n, goal, k, dp);
+    }
+};
