ea411ca1f97ad668a05b194f3384d06a407b00cd
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-08-13 20:02:48 +0530

    Time: 5 ms (51.79%) | Memory: 12.3 MB (42.38%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int n= nums.size();
-        int ans=INT_MAX;
-        int low=0, high=n-1;
-        while(low<=high){
-            int mid = low + (high - low)/2;
-            // if(nums[low]<=nums[mid]){
-            //     ans=min(ans,nums[low]);
-            //     break;
-            // }
-             if(nums[low]==nums[mid] && nums[mid]==nums[high])
-            {
-                 ans=min(ans,nums[low]);
-                low++;
-                high--;
-                continue;
-            }
-            if(nums[low]<=nums[mid]){
-                ans=min(ans,nums[low]);
-                low=mid+1;
-            }
-            else{
-                ans=min(ans, nums[mid]);
-                high=mid-1;
-            }
-        }
-        return ans;
-        // sort(nums.begin(), nums.end());
-        // return nums[0];
+        // int n= nums.size();
+        // int ans=INT_MAX;
+        // int low=0, high=n-1;
+        // while(low<=high){
+        //     int mid = low + (high - low)/2;
+        //     // if(nums[low]<=nums[mid]){
+        //     //     ans=min(ans,nums[low]);
+        //     //     break;
+        //     // }
+        //      if(nums[low]==nums[mid] && nums[mid]==nums[high])
+        //     {
+        //          ans=min(ans,nums[low]);
+        //         low++;
+        //         high--;
+        //         continue;
+        //     }
+        //     if(nums[low]<=nums[mid]){
+        //         ans=min(ans,nums[low]);
+        //         low=mid+1;
+        //     }
+        //     else{
+        //         ans=min(ans, nums[mid]);
+        //         high=mid-1;
+        //     }
+        // }
+        // return ans;
+        sort(nums.begin(), nums.end());
+        return nums[0];
     }
 };
