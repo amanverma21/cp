2ddfc710debe14319fd9da7c423acfaeb7f30afe
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-08-19 01:20:11 +0530

    Time: 94 ms (56.20%) | Memory: 39 MB (37.17%) - LeetSync

---
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<unordered_set<int>> v(n);
+
+        for(int i=0;i<roads.size();i++){
+            v[roads[i][0]].insert(roads[i][1]);
+            v[roads[i][1]].insert(roads[i][0]);
+        }
+        int ans = -1;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int curr = v[i].size()+v[j].size();
+                if(v[i].find(j)!=v[j].find(i)) curr--;
+            ans = max(ans, curr);
+            }
+        }
+        return ans;
+    }
+};
