ee761043128922c9958ee43281b658626d0e60b5
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-08-27 09:49:51 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -15,8 +15,12 @@ class Solution
     string reverseWord(string str)
     {
         // Your code goes here
-       reverse(str.begin(),str.end());
-       return str;
+        string ans="";
+        int n=str.length();
+        for(int i=n-1;i>=0;i--){
+            ans.push_back(str[i]);
+        }
+        return ans;
     }
 };
 
