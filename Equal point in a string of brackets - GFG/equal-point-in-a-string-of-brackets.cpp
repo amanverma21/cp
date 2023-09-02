729a45f7cdd14de634765b688900a37eba01fd16
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-09-02 10:30:01 +0530

    Added solution - LeetHub

---
+++ Equal point in a string of brackets - GFG/equal-point-in-a-string-of-brackets.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int findIndex(string str) {
+        // Your code goes here 
+        int cnt_close = 0;
+    int l = str.length();
+    for(int i = 0; i<l; i++){
+        if(str[i] == ')')
+            cnt_close++;
+    }
+    return cnt_close;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		Solution ob;
+		cout<<ob.findIndex(s)<<endl;
+	}
+}
+// } Driver Code Ends
