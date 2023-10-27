91375c42b039ff20b25ac45f91dc816736d7f867
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-10-27 08:53:55 +0530

    Time: 14 ms (38.46%) | Memory: 11.1 MB (23.80%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
+    int recur(int r, int i, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+        if(r==matrix.size()-1 && i<matrix[0].size() && i>=0){
+            return matrix[r][i];
+        }
+        if(i>=matrix[0].size() || i<0) return INT_MAX;
+        if(dp[r][i]!=INT_MAX)return dp[r][i];
+        
+        return dp[r][i] = matrix[r][i]+min(recur(r+1, i-1, matrix, dp), min(recur(r+1, i+1, matrix, dp), recur(r+1, i, matrix, dp)));
+    }
+    
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int minVal = INT_MAX, n = matrix.size();
-        int l, r, u;
-        if(n == 1) {
-            for(auto& cell: matrix[0]) 
-                minVal = min(minVal, cell);
-            return minVal;
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        vector<vector<int>> dp(rows, vector<int>(cols, INT_MAX));
+        
+        int mini=INT_MAX;        
+        for(int i=0;i<matrix[0].size();i++){
+            mini=min(recur(0, i, matrix, dp), mini);
         }
-        for(int i=1; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                l = (j > 0) ? matrix[i-1][j-1] : INT_MAX;
-                r = (j < n-1) ? matrix[i-1][j+1] : INT_MAX;
-                u = matrix[i-1][j];
-                matrix[i][j] += min({l, r, u});
-                if(i == n-1)
-                    minVal = min(minVal, matrix[i][j]);
-            }
-        }
-        return minVal;
+        return mini;
     }
 };
