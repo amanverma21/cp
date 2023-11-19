d2788aea20f0bccc8e2ab6a166add61c61d80b55
Aman Verma <47691773+amanverma21@users.noreply.github.com>
2023-11-19 22:49:52 +0530

    Time: 115 ms (99.08%) | Memory: 83.2 MB (59.20%) - LeetSync

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int freq[50001]={0};
+        int minN=50000, maxN=0;
+        #pragma unroll
+        for(int x: nums){
+            freq[x]++;
+            minN=min(minN, x);
+            maxN=max(maxN, x);
+        }
+        int count=0, sum=0;
+        #pragma unroll
+        for(int x=maxN; x>minN; x--){
+            if (freq[x] > 0) {
+                sum+=freq[x];
+                count+=sum;
+            }
+        }
+        return count;
+    }
+};
